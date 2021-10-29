106b95aede328e52df6d5fcf8a6cff449fba641c
theowall <theo.wall11@gmail.com>
2021-10-29 08:29:37 -0600

    removed the no tool pick up

--- public/startGameLogic.js
+++ public/startGameLogic.js
@@ -2,6 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
   let playerName = document.querySelectorAll('#Name')
 
   playerName.addEventListener('click', function() {
-    
+
   })
-}
+})
